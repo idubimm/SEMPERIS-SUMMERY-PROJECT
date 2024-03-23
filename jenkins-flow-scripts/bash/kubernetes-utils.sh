fea096125aecd0d65145603fc8470bf85573c8d4
idubi.m <idubi.m@gmail.com>
2024-03-24 01:47:48 +0200

    add replicaset and shutdown options to web app

--- jenkins-flow-scripts/bash/kubernetes-utils.sh
+++ jenkins-flow-scripts/bash/kubernetes-utils.sh
@@ -5,10 +5,10 @@ start_minikube_if_down() {
     running_count=$(echo "$minikubeStatus" | grep -o "Running" | wc -l)
     configured_count=$(echo "$minikubeStatus" | grep -o "Configured" | wc -l)
     if [[ $running_count -eq 3 && $configured_count -eq 1 ]]; then
-            echo "Status check for minikube passed , may continue woth kubernetes work"
+            echo "Status check for minikube passed --> continue with kubernetes work"
             return 0
     else 
-            echo stating minikube 
+            echo "stating minikube "
             echo `minikube start`
     fi   
 }
@@ -22,5 +22,9 @@ start_minikube_tunel_if_stopped() {
         echo "services Load Balnce is supported"
     fi
 } 
+
+get_ip_and_port_forTunneling(){
+
+}
 
 
