e352423593ff3cf24010507dd1283b66e0aebb5e
idubi.m <idubi.m@gmail.com>
2024-03-25 02:48:59 +0200

    completed 1st run infrastructure , remove access keys

---
+++ jenkins-flow-scripts/terraform/03-security-group.tf
@@ -0,0 +1,31 @@
+
+
+
+resource "aws_security_group" "sg_custom" {
+  vpc_id = aws_vpc.custom_vpc.id
+
+
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = -1
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  dynamic "ingress" {
+    for_each = var.sg_ingress_ports
+    content {
+      from_port   = ingress.value
+      to_port     = ingress.value
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  }
+
+  tags = {
+    "Name"        = "sg_custom"
+    "Description" = "custom security group"
+  }
+
+}
