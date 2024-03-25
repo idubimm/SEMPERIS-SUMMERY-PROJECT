ee2a6039aff057f1bad0b19a00c85630cf080048
idubi.m <idubi.m@gmail.com>
2024-03-25 18:59:20 +0200

    added a compose without parameters to be executed from aws on statup of machine

--- jenkins-flow-scripts/terraform/06-route-table-association-map.tf
+++ jenkins-flow-scripts/terraform/06-route-table-association-map.tf
@@ -1,5 +1,12 @@
 
-resource "aws_route_table_association" "rout_custom_association" {
+resource "aws_route_table_association" "rout_custom_association_subnete" {
   route_table_id = aws_route_table.custom_rout_table.id
-  subnet_id      = aws_subnet.public_subnet.id
+  subnet_id      = aws_subnet.public_subnet_a.id
 }
+
+resource "aws_route_table_association" "rout_custom_association_subneta" {
+  route_table_id = aws_route_table.custom_rout_table.id
+  subnet_id      = aws_subnet.public_subnet_e.id
+}
+
+
