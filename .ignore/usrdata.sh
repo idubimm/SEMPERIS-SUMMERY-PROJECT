4e0e3e603e2a9a93f52a462a47f148a9f969dcfb
idubi.m <idubi.m@gmail.com>
2024-03-25 22:00:34 +0200

    complete terraform stage 1- before ansyble

--- .ignore/usrdata.sh
+++ .ignore/usrdata.sh
@@ -4,11 +4,8 @@ user_data = <<-EOF
     sudo apt-get update
     sudo apt-get install -y docker.io
     sudo systemctl start docker
-    sudo apt  install docker-compose
     sudo apt-get install -y docker-compose
-    wget "https://raw.githubusercontent.com/idubimm/SEMPERIS-SUMMERY-PROJECT/main/src/docker-compose-image.yml" .
-    sudo docker-compose -f ./docker-compose-image.yml  up
-    echo "pulling docker compose file "
-    sudo  pull lilachamar/flask-sql-i:latest
-    sudo docker run -d -p 5000:5000 lilachamar/flask-sql-i:latest
+    echo "pulling docker compose images "
+    wget "https://raw.githubusercontent.com/idubimm/SEMPERIS-SUMMERY-PROJECT/main/src/docker-compose-flat-withoutparamters.yml" ./docker-compose-image.yml
+    sudo docker-compose -f ./docker-compose-flat-withoutparamters.yml  up
 EOF
