80af79e9777ac0aeef77717c07f9698d3906086e
idubi.m <idubi.m@gmail.com>
2024-03-25 21:59:54 +0200

    complete terraform stage 1- before ansyble

--- jenkins-flow-scripts/terraform/03-security-group.tf
+++ jenkins-flow-scripts/terraform/03-security-group.tf
@@ -1,7 +1,7 @@
 
 
 
-resource "aws_security_group" "sg_custom" {
+resource "aws_security_group" "sg_custom_public" {
   vpc_id = aws_vpc.custom_vpc.id
 
 
@@ -24,8 +24,37 @@ resource "aws_security_group" "sg_custom" {
   }
 
   tags = {
-    "Name"        = "sg_custom"
-    "Description" = "custom security group"
+    "Name"        = "sg_custom_public"
+    "Description" = "custom security public group"
+  }
+
+}
+
+resource "aws_security_group" "sg_custom_private" {
+  vpc_id = aws_vpc.custom_vpc.id
+
+
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = -1
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # dynamic "ingress" {
+  #   for_each = var.sg_ingress_ports
+  #   content {
+  #     from_port   = ingress.value
+  #     to_port     = ingress.value
+  #     protocol    = "tcp"
+  #     cidr_blocks = ["0.0.0.0/0"]
+  #   }
+  # }
+
+  tags = {
+    "Name"        = "sg_custom_private"
+    "Description" = "custom security private group"
   }
 
 }
--- jenkins-flow-scripts/terraform/06-route-table-association-map.tf
+++ jenkins-flow-scripts/terraform/06-route-table-association-map.tf
@@ -1,10 +1,10 @@
 
-resource "aws_route_table_association" "rout_custom_association_subnete" {
+resource "aws_route_table_association" "rout_custom_association_subnet_e" {
   route_table_id = aws_route_table.custom_rout_table.id
   subnet_id      = aws_subnet.public_subnet_a.id
 }
 
-resource "aws_route_table_association" "rout_custom_association_subneta" {
+resource "aws_route_table_association" "rout_custom_association_subnet_a" {
   route_table_id = aws_route_table.custom_rout_table.id
   subnet_id      = aws_subnet.public_subnet_e.id
 }
