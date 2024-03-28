98c8a3a83e566be6339c4525277c519310c59acb
idubi.m <idubi.m@gmail.com>
2024-03-29 01:49:09 +0200

    adding minikube clean cache before loading pod

--- jenkins-flow-scripts/bash/kubernetes-utils.sh
+++ jenkins-flow-scripts/bash/kubernetes-utils.sh
@@ -59,7 +59,6 @@ clear_pod_cache() {
     echo "clear_pod_cache  ==> podname=$podname"
     # if podname has value
     if [ -n "$podname" ]; then  
-        
         currentpod=$(kubectl get pods --namespace=${namepsce} | grep ${appname} | awk '{print $1}')
         echo "current pod is $currentpod" 
         echo "deleting pod ...$currentpod "
@@ -69,5 +68,7 @@ clear_pod_cache() {
         newpod=$(kubectl get pods --namespace=${namepsce} | grep ${appname} | awk '{print $1}')
         echo "replaced ${currentpod} with ${currentpod}" 
         echo `kubectl get pod ${newpod} --namespace=semperis-ns  -o yaml`
+    else;
+         echo "pod not exist , no need to clean"         
     fi
 } 
